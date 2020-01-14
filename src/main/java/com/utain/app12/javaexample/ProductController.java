e4063fc3dc180c4a903cfa6ecd648db2fd2c6c4f
utain <utainwp@gmail.com>
2020-01-14 22:06:05 +0700

    dev with docker

--- src/main/java/com/utain/app12/javaexample/ProductController.java
+++ src/main/java/com/utain/app12/javaexample/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
@@ -13,31 +15,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@RequestMapping("/")
+@RequestMapping("api/v1/products")
 @RestController
 class ProductController {
   @Autowired
   private ProductRepository productRepository;
 
   @GetMapping(value = "")
-  public List<Products> allProduct() {
-    return productRepository.findAll();
+  public Page<Products> allProduct(Pageable page) {
+    return productRepository.findAll(page);
   }
 
   @GetMapping(value = "/{id}")
-  public Optional<Products> getMethodName(@PathVariable("id") String id) {
-    return productRepository.findById(id);
+  public ResponseEntity<Products> getMethodName(@PathVariable("id") final String id) {
+    final Optional<Products> product = productRepository.findById(id);
+    if (product.isPresent()) {
+      return new ResponseEntity<>(product.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @PostMapping(value = "")
-  public Products createProduct(@RequestBody Products entity) {
-    return productRepository.save(entity);
+  public ResponseEntity<Products> createProduct(@RequestBody final Products entity) {
+    if (ObjectUtils.isEmpty(entity) || entity.getId().isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity<Products>(productRepository.save(entity), HttpStatus.CREATED);
   }
 
-  @PatchMapping(value = "")
-  public ResponseEntity<Products> postMethodName(@RequestBody Products entity) {
+  @PutMapping(value = "")
+  public ResponseEntity<Products> postMethodName(@RequestBody final Products entity) {
     if (ObjectUtils.isEmpty(entity) || entity.getId().isEmpty()) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
